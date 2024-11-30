6110abb64adf64ba3b2b2d2d0ea1d1c6e3d07bf2
Lukas Zwaller <154060189+lukaszwaller@users.noreply.github.com>
2024-11-30 08:43:18 +0100

    feat: Add mobile responsiveness

--- set-app/src/App.tsx
+++ set-app/src/App.tsx
@@ -7,16 +7,30 @@ const App: React.FC = () => {
 
   return (
     <div className="min-h-screen bg-gradient-to-br from-gray-300 via-gray-200 to-gray-100 flex flex-col items-center">
-      <h1 className="text-2xl font-bold mb-4 text-gray-800 mt-8">Set Game</h1>
-      <p className="mb-4 text-gray-700">Score: {score}</p>
-      <p className="mb-4 text-gray-700">Remaining Cards: {remainingCards}</p>
+      <h1 className="text-2xl font-bold mb-4 text-gray-800 mt-4 sm:mt-8">Set Game</h1>
+      <div className="grid grid-cols-2 gap-4 w-full">
+        <div className="flex flex-col items-center justify-center gap-2">
+          <p className="text-gray-700 font-bold w-32 py-2 ml-24">Score: {score}</p>
+          <p className="text-gray-700 font-bold w-32 py-2 ml-24">Deck: {remainingCards}</p>
+        </div>
+        <div className="flex flex-col items-center justify-center gap-2">
+          <button
+            className="bg-red-500 text-white py-2 w-32 rounded hover:bg-red-600"
+            onClick={resetGame}
+          >
+            Reset Game
+          </button>
+          <button
+            className="bg-blue-500 text-white py-2 w-32 rounded hover:bg-blue-600"
+            onClick={resetGame}
+          >
+            New Card
+          </button>
+        </div>
+      </div>
+
+
       <Board cards={cards} selectedCards={selectedCards} onSelectCard={handleCardSelection} />
-      <button
-        className="mt-4 bg-blue-500 text-white py-2 px-4 rounded hover:bg-blue-600"
-        onClick={resetGame}
-      >
-        Reset Game
-      </button>
     </div>
   );
 };
