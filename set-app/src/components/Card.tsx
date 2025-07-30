e141cda627369dd8c2af85f76afe950dca0b6fd5
Lukas Zwaller <154060189+lukaszwaller@users.noreply.github.com>
2025-07-30 22:13:12 +0200

    fix: Use Memoize to reduce rerenders

--- set-app/src/components/Card.tsx
+++ set-app/src/components/Card.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useMemo } from "react";
 import Oval from "../assets/shapes/Oval";
 import Squiggle from "../assets/shapes/Squiggle";
 import Diamond from "../assets/shapes/Diamond";
@@ -25,33 +25,41 @@ const shadingProps = {
   outlined: { fill: "transparent" },
 };
 
-const Card: React.FC<CardProps> = ({ cardId, color, shape, shading, number, isSelected, onClick }) => {
+const Card: React.FC<CardProps> = ({
+  cardId,
+  color,
+  shape,
+  shading,
+  number,
+  isSelected,
+  onClick,
+}) => {
   const borderColor = isSelected ? "border-blue-500" : "border-gray-300";
   const scale = isSelected ? "scale-105" : "hover:scale-105";
-
-  const colorClasses = {
-    red: "text-red-500",
-    green: "text-green-500",
-    purple: "text-purple-500",
-  };
-
-  const ShapeComponent = shapeComponents[shape];
   const cardSize = "w-16 h-28 sm:w-24 sm:h-40";
+
+  const colorClass = useMemo(() => {
+    const colorClasses = {
+      red: "text-red-500",
+      green: "text-green-500",
+      purple: "text-purple-500",
+    };
+    return colorClasses[color];
+  }, [color]);
+
+  const ShapeComponent = useMemo(() => shapeComponents[shape], [shape]);
+  const fill = useMemo(() => shadingProps[shading].fill, [shading]);
 
   return (
     <div
-      className={`flex flex-col justify-center items-center ${cardSize} border-2 ${borderColor} rounded-lg cursor-pointer shadow-md hover:shadow-xl ${scale} transition-all ease-in-out duration-200 ${colorClasses[color]} bg-white`}
+      className={`flex flex-col justify-center items-center ${cardSize} border-2 ${borderColor} rounded-lg cursor-pointer shadow-md hover:shadow-xl ${scale} transition-all ease-in-out duration-200 ${colorClass} bg-white`}
       onClick={onClick}
     >
-      {[...Array(number)].map((_, index) => {
-        const { fill } = shadingProps[shading];
-
-        return (
-          <div key={index} >
-            <ShapeComponent color={color} fill={fill} cardId={cardId} />
-          </div>
-        );
-      })}
+      {Array.from({ length: number }).map((_, index) => (
+        <div key={index}>
+          <ShapeComponent color={color} fill={fill} cardId={cardId} />
+        </div>
+      ))}
     </div>
   );
 };
